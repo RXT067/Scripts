d3461cc5e97c919e2f0ef0796583ce8438b58818
Kreyren <KostWarCZE@RiXotStudio.cz>
2019-01-01 18:24:13 +0000

    Update KUWAC.sh

--- KUWAC/KUWAC.sh
+++ KUWAC/KUWAC.sh
@@ -6,30 +6,9 @@
 # TODO: Create a tmp to which we will download installer?
 
 Configurator_KUWAC () {
-# TEMPORARY
 
 	if [[ $@ == @(leagueoflegends|League\ Of\ Legends) ]]; then
-		WINETRICKS_OPTIONS="corefonts vcrun2008 vcrun2017 adobeair winxp"
-		GAME="LeagueOfLegends"
-
-		while [[ ${REGION} != @(EUNE|NA|EUW|BR|LAN|LAS|OCE|RU|JP|SEA) ]]; do 
-    	echo "Select your region for League Of Legends:
-EUNE  - Europe Nordic and East
-NA    - North America
-EUW   - Europe West
-BR    - Brazil
-AN    - Latin America North
-LAS   - Latin America South
-OCE   - Oceania
-RU    - Russia
-TR    - Turkey
-JP    - Japan
-SEA   - South East Asia"
-
-    read REGION
-done
-
-		SOURCE="https://riotgamespatcher-a.akamaihd.net/releases/live/installer/deploy/League%20of%20Legends%20installer%20${REGION}.exe"
+		source <(curl -s https://raw.githubusercontent.com/RXT067/Scripts/master/KUWAC/CONFIGURATION/LeagueOfLegends.sh)
 
 		else
 			echo "$@ is not supported."
