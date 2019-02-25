9e0adf1a54de0a66401dc5c03a6deca606714acd
Jacob Hrbek <werifgx@gmail.com>
2019-02-25 12:03:00 +0100

    update

--- RKULUS/RKULUS.rs
+++ RKULUS/RKULUS.rs
@@ -1,45 +1,69 @@
-// NAME      : Rustified Krey's Universal Linux Updater Scripts (RKULUS)
-// Author    : github.com/kreyren
-// Licence   : GNUv2 (https://www.gnu.org/licenses/old-licenses/gpl-2.0.cs.html)
-// Abstract  : Rewrite KULUS (https://github.com/RXT067/Scripts/tree/master/KULUS) in rust so that i can learn it.
-// Reasoning : Using KULUS function since is seems as the most effective way to perform for learn project.
+/// NAME      : Rustified Krey's Universal Linux Updater Scripts (RKULUS)
+/// Author    : github.com/kreyren
+/// Licence   : GNUv2 (https://www.gnu.org/licenses/old-licenses/gpl-2.0.cs.html)
+/// Abstract  : Rewrite KULUS (https://github.com/RXT067/Scripts/tree/master/KULUS) in rust so that i can learn it.
+/// Reasoning : Using KULUS function since is seems as the most effective way to perform for learn project.
+/// Disclaimer: Mind that this program is expected to be full of comments used for learning.
 
-// Problems
+/// PROBLEMS
 /// Won't Compile
-//// error: failed to parse manifest at `/home/kreyren/Scripts/RKULUS/Cargo.toml`
-//// 
-//// Caused by:
-////   no targets specified in the manifest
-////   either src/lib.rs, src/main.rs, a [lib] section, or [[bin]] section must be present
+/// ---
+/// error: failed to parse manifest at `/home/kreyren/Scripts/RKULUS/Cargo.toml`
+///  
+/// Caused by:
+///    no targets specified in the manifest
+///    either src/lib.rs, src/main.rs, a [lib] section, or [[bin]] section must be present
 
-// Notes
-/// rustc <program> to compile
+/// NOTES
+/// Compilation is done using `rustc`
+/// `////` are seen as 'documentation comments'
+/// `//` are used for comments
+///
+/// Generaly you want to avoid global variables -> Slow, difficult to use in a safe way.
+/// - Slow
+/// - Difficult to use in a safe way
+/// - Don't scale with threads
+/// - Can make debugging more difficult
+/// - Can't be mutated (without tricks)
+/// <kreyren> is `static` used to set global variable?
+/// * 01 twitches
+/// <01> kreyren: geeeenerally
+/// <01> you want to avoid globals
+/// <kreyren> 01: why?
+/// <01> slow, difficult to use in a safe way
+/// <01> don't scale with threads
+/// <kreyren> i see, still need to know correct syntax (learning rust)
+/// <01> and can make debugging more difficult
+/// <01> oh. and you can't mutate them
+/// <01> (without tricks)
+/// <kreyren> noted
+/// <01> eval: static MSG: &'static str = "hello world"; MSG
+/// -eval/#rust-beginners- "hello world"
 
 // References 
-/// Video Sources
-//// Source https://youtu.be/U1EFgCNLDB8
-//// Source https://www.youtube.com/watch?v=KLMtnA2mGKs
-/// Linux Variables in Rust
-//// https://doc.rust-lang.org/std/env/index.html
+// / Video Sources
+// // Source https://youtu.be/U1EFgCNLDB8
+// // Source https://www.youtube.com/watch?v=KLMtnA2mGKs
+// / Linux Variables in Rust
+// // https://doc.rust-lang.org/std/env/index.html
 
 // Imports
 
 use std::path::Path;
-/// Check if path exists
-//// https://stackoverflow.com/questions/32384594/how-to-check-whether-a-path-exists
+// / Check if path exists
+// // https://stackoverflow.com/questions/32384594/how-to-check-whether-a-path-exists
 
-use std::process:Command; 
-/// To invoke commands
-//// https://doc.rust-lang.org/std/process/struct.Command.html
+use std::process::Command; 
+// / To invoke commands
+// // https://doc.rust-lang.org/std/process/struct.Command.html
 
-use std::env 
-/// To greb environment variables
-//// https://doc.rust-lang.org/std/env/index.html
+use std::env;
+// / To greb environment variables
+// // https://doc.rust-lang.org/std/env/index.html
 
 // Variables
-
-let something == not_nothing;
-/// Self-made example
+// Self-made example
+// static MSG: &'static str = "hello world"; // unsane
 
 // TODO: Check what system it's invoked on and what package managers are present
 
@@ -60,5 +84,3 @@ fn update_gentoo() {
  		println!("FATAL: Gentoo statement is not true..");
  	};
 }
-
-
